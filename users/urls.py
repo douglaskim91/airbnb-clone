2254578c302627f715d2409585ac8b415de8f2d2
hoon <douglaskim91@gmail.com>
2020-09-19 02:44:16 +0900

    create sign up view

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,5 @@ app_name = "users"
 urlpatterns = [
     path("login", views.LoginView.as_view(), name="login"),
     path("logout", views.log_out, name="logout"),
-    path("signup", views.SignUpView, name="sighup"),
+    path("signup", views.SignUpView.as_view(), name="signup"),
 ]
